c6b0f328a3224d88daa6015d65a273433d9d772b
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-20 19:33:27 -0500

    Parsing all relevant graffiti data and adding it to an array of hashes

--- graffiti.rb
+++ graffiti.rb
@@ -4,6 +4,17 @@ def graffiti
   # Graffiti web request
   response = HTTP.get("https://data.cityofchicago.org/resource/hec5-y4x5.json")
   all_graffiti_data = JSON.parse(response.body)
+
+  index = 0
+  needed_graffiti_data = []
+
+  # Getting relevant graffiti data
+  all_graffiti_data.each do |graffiti|
+    year = graffiti["creation_date"].slice(0..3)
+    month = graffiti["creation_date"].slice(5..6)
+    ward = graffiti["ward"]
+    needed_graffiti_data << { :year => year, :month => month, :ward => ward }
+  end
 end
 
 pp graffiti()
